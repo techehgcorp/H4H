f35183c44ca1d02eb7dbc02188f4ef5a65e8afea
wingabrielcd <wingabrielcd@gmail.com>
2025-09-20 00:15:18 -0300

    Added the analiytcs google script

--- app/[locale]/layout.jsx
+++ app/[locale]/layout.jsx
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -7,10 +8,26 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/public/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" />
         <title>Health For Haitians</title>
       </head>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {/* Google tag (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-LS0G00BZFS"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-LS0G00BZFS');
+          `}
+        </Script>
+
+        {children}
+      </body>
     </html>
   );
 }
